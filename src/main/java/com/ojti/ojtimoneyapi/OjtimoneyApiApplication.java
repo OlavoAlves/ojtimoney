f6ab0b03b5bcb56f2e33495cf7c932b5ffaeb63e
Olavo Alves Jr <olavinhoalves7@hotmail.com>
2023-06-20 15:52:35 -0300

    Comiit inicial

---
+++ src/main/java/com/ojti/ojtimoneyapi/OjtimoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.ojti.ojtimoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtimoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjtimoneyApiApplication.class, args);
+	}
+
+}
